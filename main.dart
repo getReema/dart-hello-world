2506eed7e8a70dbe1406b334fc6a664eed28cf4b
Reema <49389693+getReema@users.noreply.github.com>
2021-12-04 21:45:10 +0000

    Read user input

--- main.dart
+++ main.dart
@@ -1,23 +1,11 @@
 import "dart:math";
+import "dart:io";
 
-void main() {
+main(){
+String name = stdin.readLineSync();
+print(name);
 
-double price = 500;
-int quant = 5 ;
 
-quant = quant+4;
-quant += 4;
-quant++;
-
-print(price *quant );
-print(price / quant);
-print(price - quant);
-print(price + quant);
-print(price % quant);
-
-print(min(quant,price));
-print(max(quant,price));
-print(sqrt(200));
 
 }
 
