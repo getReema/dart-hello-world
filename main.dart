b0b6b4f56e29fdc203463aab09e68719522b8621
Reema <49389693+getReema@users.noreply.github.com>
2021-12-04 21:10:05 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,41 @@
+void main() {
+print('Hello World');
+
+print('This is Reema :)');
+
+var a = "really?";
+String var2 = "this is another variable";
+int op = 90;
+double var1 = 50.4444;
+
+
+print('a= ${a}');
+print('var2= ${var2}');
+print('op= ${op}');
+print('var1= ${var1}');
+
+}
+
+
+/*
+Course Code:
+https://gist.github.com/mikedane/bed8e31d3e2fabd6e5b7b6508d7ee15c
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
