be72c0a532c400c35cc5984dba11fe0774d63ece
Reema <49389693+getReema@users.noreply.github.com>
2021-12-04 21:20:21 +0000

    Boolean variables

--- main.dart
+++ main.dart
@@ -1,18 +1,9 @@
 void main() {
 print('Hello World');
 
-print('This is Reema :)');
+bool isSatrter = true;
 
-var a = "really?";
-String var2 = "this is another variable";
-int op = 90;
-double var1 = 50.4444;
- 
-
-print('a= ${a}');
-print('var2= ${var2}');
-print('op= ${op}');
-print('var1= ${var1}');
+print(isSatrter);
 
 }
 
